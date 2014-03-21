2ac0d9519b8956a8b6e9e2a07af68a917bdd3991
fmacfarlane <f.macfarlane@dundee.ac.uk>
2014-03-21 19:00:51 +0000

    Script used to complete assignment 4

--- assignment_4script.R
+++ assignment_4script.R
@@ -7,20 +7,14 @@ require(picante)
 require(phytools)
 #Read in the data from the fasta file.
 x<-read.dna("marsup_rag1.fasta",format="fasta")
-x<-as.phyDat(x)
+#change the rownames to account for doubles
 rownames(x)<-((1:103))
 
-rownames(x)
-#Compare the number of substitutions seperating any pair of sequences(distance)
+#Compare the number of substitutions seperating any pair of sequences(distance), this uses deafult K80
 d<-dist.dna(x)
+
 #Write this into a viewable table
 write.table(as.matrix(d),"distances1.csv")
-#Select a substitution model to use, in this case GG95.
-d2<-dist.dna(x, model="k80")
-#write this to a viewable table
-write.table(as.matrix(d2),"distances2.csv")
-#
-#
 #
 #Plotting trees from distance matrices
 #
@@ -33,13 +27,7 @@ tr.bionjr<-root(tr.bionj,outgroup="1", resolve.root=TRUE )
 #plot the rooted tree
 plot(tr.bionjr);add.scale.bar(length=0.001)
 #
-#
-#
-#
-#
-#
 #Tree distortion
-#
 #
 #Calculate the distortion using cophenetic analysis
 dt.bionj<-cophenetic(tr.bionj)
@@ -55,106 +43,11 @@ dt.bionj<-as.dist(dt.bionj)
 plot(dt.bionj-d,ylab="residuals", cex=0.5,main="BIONJ")
 abline(h=0,lty=3)
 #
-#
-#BOOTSTRAPPING
-#
-#Fit the tree to the data
-fit<-pml(tr.bionj,as.phyDat(x))
-#optimise plot and set random seed for bootstrap process
-fit=optim.pml(fit,T)
-plot(fit)
-set.seed(8)
-#bootstrap the data and plot the results
-bs<-bootstrap.pml(fit,bs=100,optNni=T)
-treeBS<-plotBS(fit$tree, type="p", bs)
-#
-#Substitution Models: find the most appropriate model
-mt<-modelTest(as.phyDat(x),G=F,I=F)
-View(mt)#pick lowest AIC values
-#lowest AIC value is GTR model
-#This is the best model, ,recalculate distance with this method.
-#
-#PLOT GTR TREES with BIONJ
-#unrooted
-x<-as.phyDat(x)
-obj <- pml(tree = rtree(n = length(x), tip.label = names(x), rooted = FALSE), 
-           data = x)
-obj
-fit.gtr <- optim.pml(obj, optNni = TRUE, optEdge = TRUE, model = "GTR")
-fit.gtr
-plotTree(midpoint(fit.gtr$tree))
-
-#
-#rooted
-objr <- pml(tree = rtree(n = length(x), tip.label = names(x), rooted = TRUE), 
-data = x)
-objr
-fit.gtrr <- optim.pml(obj, optNni = TRUE, optEdge = TRUE, model = "GTR")
-fit.gtrr
-gtrur<-plotTree(midpoint(fit.gtr$tree))
-#
-#
-#
-#distance with gtr
-dm = dist.ml(x, model="en")
-write.table(as.matrix(dm),"distances3.csv")
-tr.gbionj<-bionj(dm)
-#plot the tree
-plot(tr.gbionj)
-#root the tree at the node=outgroup
-tr.gbionjr<-root(tr.gbionj,outgroup="1", resolve.root=TRUE )
-#plot the rooted tree
-plot(tr.gbionjr);add.scale.bar(length=0.001)
-#
-#
-#
-#
-#
-#
-#Tree distortion
-#
-#
-#Calculate the distortion using cophenetic analysis
-dt.gbionj<-cophenetic(tr.gbionj)
-#pull out the taxa data as a matrix
-dmat<-as.matrix(dm)
-#pul out the rownames of dmat and assign value
-nms<-rownames(dmat)
-#update the cophenetic analysis with the rownames
-dt.gbionj<-dt.gbionj[nms, nms]
-#Calculate the distances
-dt.gbionj<-as.dist(dt.gbionj)
-#plot new distance - old distance = residuals
-plot(dt.gbionj-dm,ylab="residuals", cex=0.5,main="GTR-BIONJ")
-abline(h=0,lty=3)
-
-#
-#Bootrstrap with GTR
-env = attr(mt, "env")
-fitStart = eval(get("GTR", env), env)
-fitStart = eval(get(mt$Model[which.min(mt$BIC)], env), env)
-fit = optim.pml(fitStart, optNni=TRUE, optGamma=TRUE, optInv=TRUE,
-                model="GTR")
-plot(fit)
-set.seed(8)
-bs<-bootstrap.pml(fit,bs=100,optNni=T)
-treeBS<-plotBS(fit$tree, type="p", bs)
-
-
-
-#BAYES
-#
-#ED scores
-#
-#Calculate the evolutionary distinctiveness of ech gene (must be done on a rooted tree)
-orig<-evol.distinct(tr.bionjr,type="fair.proportion")
-orig
-
 
 #Redo for nj tree
 #Plotting trees from distance matrices
 #
-#choose method of construction(bionj in this case)
+#choose method of construction(nj in this case)
 tr.nj<-nj(d)
 #plot the tree
 plot(tr.nj)
@@ -183,7 +76,6 @@ plot(dt.nj-d,ylab="residuals", cex=0.5,main="NJ")
 abline(h=0,lty=3)
 #
 #
-par(mfrow=c(1,2))
 #redo with upgma
 #choose method of construction(upgma in this case)
 tr.upgma<-upgma(d)
@@ -213,5 +105,56 @@ dt.upgma<-as.dist(dt.upgma)
 plot(dt.upgma-d,ylab="residuals", cex=0.5,main="upgma")
 abline(h=0,lty=3)
 #
-#
+#compare the 3
 par(mfrow=c(1,3))
+plot(dt.upgma-d,ylab="residuals", cex=0.5,main="upgma")
+abline(h=0,lty=3)
+plot(dt.nj-d,ylab="residuals", cex=0.5,main="nj")
+abline(h=0,lty=3)
+plot(dt.bionj-d,ylab="residuals", cex=0.5,main="bionj")
+abline(h=0,lty=3)
+#Bionj has lowest residuals
+#
+#BOOTSTRAPPING
+#
+#Fit the tree to the data
+fit<-pml(tr.bionj,as.phyDat(x))
+#optimise plot and set random seed for bootstrap process
+fit=optim.pml(fit,T)
+plot(fit)
+set.seed(8)
+#bootstrap the data and plot the results
+bs<-bootstrap.pml(fit,bs=100,optNni=T)
+treeBS<-plotBS(fit$tree, type="p", bs)
+#
+#Substitution Models: find the most appropriate model
+mt<-modelTest(as.phyDat(x),G=F,I=F)
+View(mt)#pick lowest AIC values
+#lowest AIC value is GTR model
+#This is the best model, ,recalculate distance with this method.
+#
+fittedtreeGTR<-pml(tr.bionj,as.phyDat(x),k=4,inv=0.2)
+plot(fittedtreeGTR)
+#boostrap
+bs<-bootstrap.pml(fittedtreeGTR,bs=100,optNni=T)
+plotBS(fittedtreeGTR$tree,type='p',bs)
+#
+#cophenetic analysis
+dt.fittedtreeGTR<-cophenetic(fittedtreeGTR$tree)
+dt.fittedtreeGTR<-dt.fittedtreeGTR[nms, nms]
+#Calculate the distances
+dt.fittedtreeGTR<-as.dist(dt.fittedtreeGTR)
+#plot new distance - old distance = residuals
+plot(dt.fittedtreeGTR-d,ylab="residuals", cex=0.5,main="GTR")
+abline(h=0,lty=3)
+
+#BAYES
+#Was done on xming
+#ED scores
+#
+#Calculate the evolutionary distinctiveness of ech gene (must be done on a rooted tree)
+orig<-evol.distinct(tr.bionjr,type="fair.proportion")
+orig
+View(orig)
+#plot histogram
+ plot(orig, col=" blue", border=" blue" ,main="ED score", density=100)
