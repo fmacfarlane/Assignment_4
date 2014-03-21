62688bc0a24223b1b7d0cbff755cc6c8460b1dbd
fmacfarlane <f.macfarlane@dundee.ac.uk>
2014-03-21 09:52:07 +0000

    R script

---
+++ assignment_4script.R
@@ -0,0 +1,217 @@
+#install.packages("ape")
+#install.packages("phangorn")
+#install.packages("picante")
+require(ape)
+require(phangorn)
+require(picante)
+require(phytools)
+#Read in the data from the fasta file.
+x<-read.dna("marsup_rag1.fasta",format="fasta")
+x<-as.phyDat(x)
+rownames(x)<-((1:103))
+
+rownames(x)
+#Compare the number of substitutions seperating any pair of sequences(distance)
+d<-dist.dna(x)
+#Write this into a viewable table
+write.table(as.matrix(d),"distances1.csv")
+#Select a substitution model to use, in this case GG95.
+d2<-dist.dna(x, model="k80")
+#write this to a viewable table
+write.table(as.matrix(d2),"distances2.csv")
+#
+#
+#
+#Plotting trees from distance matrices
+#
+#choose method of construction(bionj in this case)
+tr.bionj<-bionj(d)
+#plot the tree
+plot(tr.bionj)
+#root the tree at the node=outgroup
+tr.bionjr<-root(tr.bionj,outgroup="1", resolve.root=TRUE )
+#plot the rooted tree
+plot(tr.bionjr);add.scale.bar(length=0.001)
+#
+#
+#
+#
+#
+#
+#Tree distortion
+#
+#
+#Calculate the distortion using cophenetic analysis
+dt.bionj<-cophenetic(tr.bionj)
+#pull out the taxa data as a matrix
+dmat<-as.matrix(d)
+#pul out the rownames of dmat and assign value
+nms<-rownames(dmat)
+#update the cophenetic analysis with the rownames
+dt.bionj<-dt.bionj[nms, nms]
+#Calculate the distances
+dt.bionj<-as.dist(dt.bionj)
+#plot new distance - old distance = residuals
+plot(dt.bionj-d,ylab="residuals", cex=0.5,main="BIONJ")
+abline(h=0,lty=3)
+#
+#
+#BOOTSTRAPPING
+#
+#Fit the tree to the data
+fit<-pml(tr.bionj,as.phyDat(x))
+#optimise plot and set random seed for bootstrap process
+fit=optim.pml(fit,T)
+plot(fit)
+set.seed(8)
+#bootstrap the data and plot the results
+bs<-bootstrap.pml(fit,bs=100,optNni=T)
+treeBS<-plotBS(fit$tree, type="p", bs)
+#
+#Substitution Models: find the most appropriate model
+mt<-modelTest(as.phyDat(x),G=F,I=F)
+View(mt)#pick lowest AIC values
+#lowest AIC value is GTR model
+#This is the best model, ,recalculate distance with this method.
+#
+#PLOT GTR TREES with BIONJ
+#unrooted
+x<-as.phyDat(x)
+obj <- pml(tree = rtree(n = length(x), tip.label = names(x), rooted = FALSE), 
+           data = x)
+obj
+fit.gtr <- optim.pml(obj, optNni = TRUE, optEdge = TRUE, model = "GTR")
+fit.gtr
+plotTree(midpoint(fit.gtr$tree))
+
+#
+#rooted
+objr <- pml(tree = rtree(n = length(x), tip.label = names(x), rooted = TRUE), 
+data = x)
+objr
+fit.gtrr <- optim.pml(obj, optNni = TRUE, optEdge = TRUE, model = "GTR")
+fit.gtrr
+gtrur<-plotTree(midpoint(fit.gtr$tree))
+#
+#
+#
+#distance with gtr
+dm = dist.ml(x, model="en")
+write.table(as.matrix(dm),"distances3.csv")
+tr.gbionj<-bionj(dm)
+#plot the tree
+plot(tr.gbionj)
+#root the tree at the node=outgroup
+tr.gbionjr<-root(tr.gbionj,outgroup="1", resolve.root=TRUE )
+#plot the rooted tree
+plot(tr.gbionjr);add.scale.bar(length=0.001)
+#
+#
+#
+#
+#
+#
+#Tree distortion
+#
+#
+#Calculate the distortion using cophenetic analysis
+dt.gbionj<-cophenetic(tr.gbionj)
+#pull out the taxa data as a matrix
+dmat<-as.matrix(dm)
+#pul out the rownames of dmat and assign value
+nms<-rownames(dmat)
+#update the cophenetic analysis with the rownames
+dt.gbionj<-dt.gbionj[nms, nms]
+#Calculate the distances
+dt.gbionj<-as.dist(dt.gbionj)
+#plot new distance - old distance = residuals
+plot(dt.gbionj-dm,ylab="residuals", cex=0.5,main="GTR-BIONJ")
+abline(h=0,lty=3)
+
+#
+#Bootrstrap with GTR
+env = attr(mt, "env")
+fitStart = eval(get("GTR", env), env)
+fitStart = eval(get(mt$Model[which.min(mt$BIC)], env), env)
+fit = optim.pml(fitStart, optNni=TRUE, optGamma=TRUE, optInv=TRUE,
+                model="GTR")
+plot(fit)
+set.seed(8)
+bs<-bootstrap.pml(fit,bs=100,optNni=T)
+treeBS<-plotBS(fit$tree, type="p", bs)
+
+
+
+#BAYES
+#
+#ED scores
+#
+#Calculate the evolutionary distinctiveness of ech gene (must be done on a rooted tree)
+orig<-evol.distinct(tr.bionjr,type="fair.proportion")
+orig
+
+
+#Redo for nj tree
+#Plotting trees from distance matrices
+#
+#choose method of construction(bionj in this case)
+tr.nj<-nj(d)
+#plot the tree
+plot(tr.nj)
+#root the tree at the node=outgroup
+tr.njr<-root(tr.nj,outgroup="1", resolve.root=TRUE )
+#plot the rooted tree
+plot(tr.nj);add.scale.bar(length=0.001)
+#
+#
+#
+#Tree distortion
+#
+#
+#Calculate the distortion using cophenetic analysis
+dt.nj<-cophenetic(tr.nj)
+#pull out the taxa data as a matrix
+dmat<-as.matrix(d)
+#pul out the rownames of dmat and assign value
+nms<-rownames(dmat)
+#update the cophenetic analysis with the rownames
+dt.nj<-dt.nj[nms, nms]
+#Calculate the distances
+dt.nj<-as.dist(dt.nj)
+#plot new distance - old distance = residuals
+plot(dt.nj-d,ylab="residuals", cex=0.5,main="NJ")
+abline(h=0,lty=3)
+#
+#
+par(mfrow=c(1,2))
+#redo with upgma
+#choose method of construction(upgma in this case)
+tr.upgma<-upgma(d)
+#plot the tree
+plot(tr.upgma)
+#root the tree at the node=outgroup
+tr.upgmar<-root(tr.upgma,outgroup="1", resolve.root=TRUE )
+#plot the rooted tree
+plot(tr.upgmar);add.scale.bar(length=0.001)
+#
+#
+#
+#Tree distortion
+#
+#
+#Calculate the distortion using cophenetic analysis
+dt.upgma<-cophenetic(tr.upgma)
+#pull out the taxa data as a matrix
+dmat<-as.matrix(d)
+#pul out the rownames of dmat and assign value
+nms<-rownames(dmat)
+#update the cophenetic analysis with the rownames
+dt.upgma<-dt.upgma[nms, nms]
+#Calculate the distances
+dt.upgma<-as.dist(dt.upgma)
+#plot new distance - old distance = residuals
+plot(dt.upgma-d,ylab="residuals", cex=0.5,main="upgma")
+abline(h=0,lty=3)
+#
+#
+par(mfrow=c(1,3))
